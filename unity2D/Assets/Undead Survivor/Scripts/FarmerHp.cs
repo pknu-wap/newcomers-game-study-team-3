af7657b754638f58ce691256b56c51a2e9e2992a
chaeyun0426-dev <chaeyun0426@gmail.com>
2026-03-09 13:41:08 +0900

    fix: Hp Damage 적 인스턴스로 수정

--- unity2D/Assets/Undead Survivor/Scripts/FarmerHp.cs
+++ unity2D/Assets/Undead Survivor/Scripts/FarmerHp.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
-public class FarmerHp:MonoBehaviour
+public class FarmerHp : MonoBehaviour
 {
-    private const float MaxHp=100f;
-    private const float Damage=0.5f;
-    private float _currentHp=MaxHp;
+    private const float MaxHp = 100f;
+    private float _currentHp = MaxHp;
 
-    private void OnTriggerStay2D(Collider2D other)
+    public void TakeDamage(float damage)
     {
-        _currentHp-=Damage;
-        Debug.Log(_currentHp);
+        _currentHp -= damage;
+        Debug.Log("농부 체력: " + _currentHp);
     }
 }
