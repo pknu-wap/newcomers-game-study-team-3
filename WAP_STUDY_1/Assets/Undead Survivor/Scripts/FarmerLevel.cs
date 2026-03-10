ef3b3fc2bb96d9b6f02f08e73021ba7226660bb7
khw3268480 <khw3268480@naver.com>
2026-03-10 17:53:28 +0900

    feat: 레벨 업 UI 추가, 스크립트 구현 필요

--- WAP_STUDY_1/Assets/Undead Survivor/Scripts/FarmerLevel.cs
+++ WAP_STUDY_1/Assets/Undead Survivor/Scripts/FarmerLevel.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class FarmerLevel : MonoBehaviour
 {
+    [SerializeField] private GameObject levelUpPanel;
+
+
     private int _currentExp;
     private int _level = 1;
     private int _maxExp = 50;
@@ -15,10 +18,18 @@ public class FarmerLevel : MonoBehaviour
             _level++;
             // 경험치 통 레벨의 50배수로 증가.
             _maxExp = _level * 50;
-            
+
             TimeController.PauseGame();
+
+            levelUpPanel.SetActive(true);
         }
-        
-        Debug.Log("레벨: "+_level+" 경험치: "+_currentExp);
+
+        Debug.Log("레벨: " + _level + " 경험치: " + _currentExp);
+    }
+    
+    public void LevelUp(int index)
+    {
+        levelUpPanel.SetActive(false);
+        TimeController.ResumeGame();
     }
 }
