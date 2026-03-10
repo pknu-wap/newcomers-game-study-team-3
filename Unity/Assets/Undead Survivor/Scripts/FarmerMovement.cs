004c89bd3c71852505632fde23f118e7bd0ed356
fish7576 <146370277+fish7576@users.noreply.github.com>
2026-03-10 18:03:29 +0900

    add animation

--- Unity/Assets/Undead Survivor/Scripts/FarmerMovement.cs
+++ Unity/Assets/Undead Survivor/Scripts/FarmerMovement.cs
@@ -5,12 +5,33 @@ public class FarmerMovement : MonoBehaviour
 {
     private const float Speed = 10f;
     private Vector2 _moveDirection;
-
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private void Awake()
+    {
+        // GetComponent는 여기서 한 번만
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         Vector2 move = _moveDirection.normalized;
         transform.position += (Vector3)(move * Speed * Time.deltaTime);
+        
+        if (_moveDirection.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_moveDirection.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+        bool isMoving = _moveDirection.sqrMagnitude > 0.01f;
+        _animator.SetBool("isMoving", isMoving);
+
     }
+
 
     private void OnMove(InputValue value)
     {
